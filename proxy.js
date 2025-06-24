607751f158b8016607ab706e521c65c1efcd0322
Sylle90445 <S.Ritzmann@etm-international.de>
2025-06-24 09:14:37 +0200

    Update proxy.js

--- proxy.js
+++ proxy.js
@@ -11,6 +11,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use(express.static(__dirname));
+
 const DB_PATH = path.join(__dirname, 'db.json');
 
 function readUsers() {

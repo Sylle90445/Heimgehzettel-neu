10c9492c5eb5400cd21c1a34a5b5ff7c9d8d99e7
Sylle90445 <S.Ritzmann@etm-international.de>
2025-06-24 07:54:45 +0200

    Update proxy.js

--- proxy.js
+++ proxy.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 const cors = require('cors');
+const port = process.env.PORT || 4000
 
 const app = express();
 const PORT = process.env.PORT || 3000;
